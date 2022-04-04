2e64c8a75ceb10387ae1ea30579fbb359407db80
ojisan <ojisan@syneforge.com>
2022-04-04 20:03:16 +0300

    Update

---
+++ cmd/web/handlers.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	files := []string{
+		"./ui/html/home.page.tpl",
+		"./ui/html/base.layout.tpl",
+		"./ui/html/footer.partial.tpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+	}
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	_, err := w.Write([]byte(fmt.Sprintf("Your are now in %s", r.RequestURI)))
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	_, err = fmt.Fprintf(w, "Displaying a snippet with id: %d", id)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
---
+++ cmd/web/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	log.Println("Server listening in port :8000")
+	err := http.ListenAndServe(":8000", mux)
+	log.Fatal(err)
+}
